0a0d153e7b0baa93200952b45d58ac2fd512bb72
Yuki Naka <yukinaka2168@au.com>
2024-10-25 16:20:14 +0900

    「習い事を開始画面」を表示する関数「StartScreen」内に「習い事を開始画面」を構成するUIの関数を一旦全て記述。

--- app/src/main/java/com/example/lessonscalendar/StartFragment.kt
+++ app/src/main/java/com/example/lessonscalendar/StartFragment.kt
@@ -6,7 +6,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
@@ -17,11 +21,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 import com.example.lessonscalendar.ui.theme.LessonsCalendarTheme
 
@@ -41,9 +47,23 @@ class StartFragment : Fragment() {
 
 @Composable
 fun StartScreen() {
-    Text(
-        text = "習い事を開始用画面"
-    )
+    Row {
+        Column(
+            modifier = Modifier
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            WhatToLearnText()
+            WhatToLearnTextField()
+            WhenToLearnText()
+            WhenToLearnPicker()
+            MonthlyCostText()
+            MonthlyCostInputField()
+            YenText()
+            StartButton()
+        }
+    }
 }
 
 @Preview(showBackground = true)
