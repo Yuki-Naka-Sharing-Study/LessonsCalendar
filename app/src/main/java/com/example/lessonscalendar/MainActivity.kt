063fb1636de49c86fc00a2773303641d6e1be45f
Yuki Naka <yukinaka2168@au.com>
2024-10-25 14:18:19 +0900

    「MainActivity」に「BottomNavigation」関連のコードを作成。

--- app/src/main/java/com/example/lessonscalendar/MainActivity.kt
+++ app/src/main/java/com/example/lessonscalendar/MainActivity.kt
@@ -3,44 +3,91 @@ package com.example.lessonscalendar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.lessonscalendar.ui.theme.LessonsCalendarTheme
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LessonsCalendarTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MyApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomNavigationBar(navController) }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = "confirm",
+
+            Modifier.padding(innerPadding)
+        ) {
+            composable("confirm") { ConfirmScreen() }
+            composable("start") { StartScreen() }
+            composable("setting") { SettingScreen() }
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LessonsCalendarTheme {
-        Greeting("Android")
+fun BottomNavigationBar(navController: NavController) {
+    val navBackStackEntry = navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry.value?.destination
+
+    BottomNavigation {
+        BottomNavigationItem(
+            icon = { Icon(Icons.Default.Check, contentDescription = "Confirm") },
+            label = { Text("Confirm") },
+            selected = currentDestination?.route == "confirm",
+            onClick = {
+                navController.navigate("confirm") {
+//                    popUpTo("confirm") { saveState = true }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
+        )
+        BottomNavigationItem(
+            icon = { Icon(Icons.Default.Edit, contentDescription = "Start") },
+            label = { Text("Start") },
+            selected = currentDestination?.route == "start",
+            onClick = {
+                navController.navigate("start") {
+//                    popUpTo("record") { saveState = true }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
+        )
+        BottomNavigationItem(
+            icon = { Icon(Icons.Default.Settings, contentDescription = "Settings") },
+            label = { Text("Settings") },
+            selected = currentDestination?.route == "setting",
+            onClick = {
+                navController.navigate("setting") {
+//                    popUpTo("setting") { saveState = true }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
+        )
     }
 }
